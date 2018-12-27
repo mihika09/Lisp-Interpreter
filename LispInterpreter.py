f7585aee1a9d408af5bffb7deb7c3bd54d27cfa3
mihika09 <mohta.mihika@gmail.com>
2018-12-28 02:58:56 +0530

    Initial Draft for evaluator

--- LispInterpreter.py
+++ LispInterpreter.py
@@ -1,16 +1,83 @@
 import math
 import operator as op
+from collections import ChainMap as Environment
 
-check_whitespace = [' ', '\n', '\t']
+
+"""class Procedure(object):
+    # "A user-defined Scheme procedure."
+
+    def __init__(self, parms, body, env):
+        self.parms, self.body, self.env = parms, body, env
+
+    def __call__(self, *args):
+        env =  Environment(dict(zip(self.parms, args)), self.env)
+        return eval(self.body, env)"""
 
 
-"""def remove_whitespace(s):
+def standard_env():
 
-    i = 0
-    while i < len(s) and s[i] in check_whitespace:
-        i += 1
-    s = s[i:]
-    return s"""
+    env = {}
+    env.update(vars(math))  # sin, cos, sqrt, pi, ...
+    env.update({
+        '+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv,
+        '>': op.gt, '<': op.lt, '>=': op.ge, '<=': op.le, '=': op.eq,
+        'abs': abs,
+        'append': op.add,
+        'apply': lambda proc, args: proc(*args),
+        'begin': lambda *x: x[-1],
+        'car': lambda x: x[0],
+        'cdr': lambda x: x[1:],
+        'cons': lambda x, y: [x] + y,
+        'eq?': op.is_,
+        'equal?': op.eq,
+        'length': len,
+        'list': lambda *x: list(x),
+        'list?': lambda x: isinstance(x,list),
+        'map': lambda *args: list(map(*args)),
+        'max': max,
+        'min': min,
+        'not': op.not_,
+        'null?': lambda x: x == [],
+        'number?': lambda x: isinstance(x, (int, float)),
+        'procedure?': callable,
+        'round': round,
+        'symbol?': lambda x: isinstance(x, str),
+    })
+    return env
+
+
+def eval(x, env):
+
+    print("x: ", x)
+
+    if isinstance(x, str):  # variable reference
+        return env[x]
+
+    elif not isinstance(x, list):  # constant literal
+        return x
+
+    elif isinstance(x, (int, float)):      # constant number
+        return x
+
+    elif x[0] == 'if':               # conditional
+        (_, test, conseq, alt) = x
+        exp = (conseq if eval(test, env) else alt)
+        return eval(exp, env)
+
+    elif x[0] == 'define':           # definition
+        (_, symbol, exp) = x
+        env[symbol] = eval(exp, env)  # adding a new key in env with var_name as key name and the corresponding value
+
+    else:
+
+        print("x[0]: ", x[0])
+        proc = eval(x[0], env)
+        print("proc: ", proc)
+
+        args = [eval(arg, env) for arg in x[1:]]
+        print("args: ", args)
+        print("proc: ", proc)
+        return proc(*args)
 
 
 def atom(x):
@@ -43,7 +110,7 @@ def parser(token, s):
         print("Unexpected EOF")
         return None
 
-    s = s[1:]
+    # s = s[1:]
     if token == '(':
 
         lst = []
@@ -80,7 +147,10 @@ if __name__ == '__main__':
     if ps is not None:
         s = ps[1].strip()
         if len(s) == 0:
+            env = standard_env()
             print(ps[0])
+            # print(eval(ps[0], env))
+
         else:
             print("Unexpected: ", s)
 
