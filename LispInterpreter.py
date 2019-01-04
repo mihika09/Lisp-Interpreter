ea46aa009237afa0dbde1695fe023c158a82c866
mihika09 <mohta.mihika@gmail.com>
2019-01-04 09:01:08 +0530

    Implemented Lambda, nested not working

--- LispInterpreter.py
+++ LispInterpreter.py
@@ -4,7 +4,7 @@ import operator as op
 from collections import ChainMap as Environment
 
 special_strings = ["define", "if", "lambda"]
-sys.setrecursionlimit(2000)
+sys.setrecursionlimit(3000)
 
 
 class Procedure(object):
@@ -14,7 +14,8 @@ class Procedure(object):
 
     def __call__(self, *args):
         env = Environment(dict(zip(self.parms, args)), self.env)
-        px, sx = parse_eval(self.body)
+        x, t = get_identifier(self.body)
+        px, sx = eval_exp(x, t, env)
         return px
 
 
@@ -78,7 +79,8 @@ def num_parser(token):
     except ValueError:
         try:
             return float(token)
-        except ValueError: return str(token)
+        except ValueError:
+            return str(token)
 
 
 def fun(attr, temp, s):
@@ -137,29 +139,94 @@ def if_parser(s, env):
 
 def define_parser(s, env):
     var, s = get_identifier(s)
+    print("var: ", var)
     exp, s = get_identifier(s)
+    print("exp: ", exp)
     exp_eval, s = eval_exp(exp, s, env)
+    print("exp_eval: ", exp_eval)
     env[var] = exp_eval
+    print("var: ", var)
+    print("env[var]: ", env[var])
+    print("s_define: ", s)
     return None, s
+
+
+def funl(attr, temp, s):
+
+    count = 1
+    while count != 0:
+
+        attr = attr + temp + ' '
+        temp, s = get_identifier(s)
+        if temp == '(':
+            count += 1
+        elif temp == ')':
+            count -= 1
+
+    attr += ')'
+    print("attr_lambda: ", attr, "s: ", s)
+    return attr, s
 
 
 def get_lambda_attr(s):
     attr, s = get_identifier(s)
     if attr == '(':
-        attr, s = fun(attr, '', s)
+        attr, s = funl(attr, '', s)
         attr = attr.replace('(', ' ( ').replace(')', ' ) ')
         # print("attr after: ", attr)
 
     return attr, s
 
 
-def lambda_parser(s):
+def lambda_parser(s, env):
 
+    print("Lambda Parser")
     parms, s = get_lambda_attr(s)
     body, s = get_lambda_attr(s)
     parms = list(parms.split())
     parms = parms[1]
-    return Procedure(parms, body), s
+    print("parms: ", parms, "body: ", body, "s: ", s)
+    return Procedure(parms, body, env), s
+
+
+def quote_parser(s, env):
+
+    print("Quote Parser")
+    attr, s = get_identifier(s)
+
+    if attr == '(':
+        attr = s[0:s.index(')')]
+        s = s[s.index(')')+1:]
+        print("attr: ", attr)
+
+    return attr, s
+
+
+def get_args(s, env):
+
+    iden, s = get_identifier(s)
+    args = []
+    print("iden: ", iden, "args: ", args)
+
+    while len(iden) > 0 and iden != ')':
+        print("tokena: ", iden, "s: ", s)
+        try:
+            x = env[iden]
+            print("x: ", x)
+            args.append(x)
+
+        except KeyError:
+            x, s = eval_exp(iden, s, env)
+            args.append(x)
+
+        print("args: ", args)
+        iden, s = get_identifier(s)
+
+    if iden != ')':
+        print("Invalid InputMissings )")
+        return None, s
+
+    return args, s
 
 
 def eval_exp(iden, s, env):
@@ -170,22 +237,51 @@ def eval_exp(iden, s, env):
 
         if iden == '(':
             iden, s = get_identifier(s)
-            return eval_exp(iden, s, env)
+            proc, s = eval_exp(iden, s, env)
+            print("p: ", proc, "s: ", s)
+            args, s = get_args(s, env)
+
+            if args is None:
+                return None, s
+
+            elif len(args) == 0:
+                iden = ')'
+
+            else:
+                print("proc: ", proc, "args: ", args, "s: ", s)
+                try:
+                    y = proc(*args)
+                    print("y p(*a): ", y, "s: ", s)
+                except TypeError:
+                    print("Type Error")
+                    return None, s
+
+                print("y: ", y, "s: ", s)
+                return y, s
 
         elif iden == 'define':
             proc, s = define_parser(s, env)
-            iden, s = get_identifier(s)
-            print("iden: ", iden, "s: ", s)
+            # iden, s = get_identifier(s)
+            return proc, s
+            print("iden_define: ", iden, "s: ", s)
 
         elif iden == 'if':
             proc, s = if_parser(s, env)
-            iden, s = get_identifier(s)
-            print("proc_if :", proc, "s: ",s)
+            # iden, s = get_identifier(s)
+
+            print("proc_if :", proc, "s: ", s)
+            return proc, s
 
         elif iden == 'lambda':
-            proc, s = lambda_parser(s)
-            iden, s = get_identifier(s)
+            proc, s = lambda_parser(s, env)
+
             print("proc_lambda: ", proc, "s: ", s)
+            return proc, s
+
+        elif iden == 'quote':
+            proc, s = quote_parser(s, env)
+            print("proc: ", proc, "s: ", s)
+            iden, s = get_identifier(s)
 
         else:
             x = num_parser(iden)
@@ -206,7 +302,7 @@ def eval_exp(iden, s, env):
                     args = []
                     print("iden: ", iden, "args: ", args)
                     while len(iden) > 0 and iden != ')':
-                        print("tokena: ", iden)
+                        print("tokena: ", iden, "s: ", s)
                         try:
                             x = env[iden]
                             print("x: ", x)
@@ -223,9 +319,10 @@ def eval_exp(iden, s, env):
                         print("Invalid InputMissing )")
                         return None, s
 
-                    print("proc: ", proc, "args: ", args)
+                    print("proc: ", proc, "args: ", args, "s: ", s)
                     try:
                         y = proc(*args)
+                        print("y p(*a): ", y, "s: ", s)
                     except TypeError:
                         print("Type Error")
                         return None, s
@@ -234,7 +331,7 @@ def eval_exp(iden, s, env):
                     return y, s
 
                 except KeyError:
-                    print("Error: Unbound symbol \'{}\'".format(x))
+                    print("Error: Unbound symbols \'{}\'".format(x))
                     return None, s
 
     return proc, s
@@ -245,7 +342,7 @@ def parse_eval(s, env):
     iden, s = get_identifier(s)
 
     if iden == '(':
-        iden, s = get_identifier(s)
+        # iden, s = get_identifier(s)
         x, y = eval_exp(iden, s, env)
         print("x: ", x, "y: ", y)
         return x, y
@@ -262,9 +359,11 @@ def parse_eval(s, env):
 
         else:
             try:
+                print("x: ", x)
+                print("env[x]: ", env[x])
                 return env[x], s
             except KeyError:
-                print("Error: Unbound symbol \'{}\'".format(x))
+                print("Error: Unbound symbols \'{}\'".format(x))
                 return None, s
 
 
@@ -272,11 +371,10 @@ if __name__ == '__main__':
 
     def repl():
 
+        global_env = standard_env()
         while True:
 
             s = input('>> ').replace('(', ' ( ').replace(')', ' ) ')
-
-            global_env = standard_env()
 
             if s == '':
                 exit()
